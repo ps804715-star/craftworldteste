d8c4b763cefa087cb58632705c868a4b18a97e57
ps804715-star <ps804715@gmail.com>
2026-04-29 13:43:15 +0100

    Add files via upload

---
+++ COMPONENTS/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="card" style={{ textAlign: 'center', padding: '30px' }}>
+      <h1 style={{ fontSize: '32px', color: '#00d9ff', marginBottom: '10px' }}>
+        🎮 CW Game Tracker Pro
+      </h1>
+      <p style={{ color: '#888' }}>
+        Gestão Industrial & Análise Financeira
+      </p>
+    </div>
+  )
+}
+
+export default Header
